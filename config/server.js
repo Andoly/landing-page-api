18c5e15ab0ae736cd71f1f6d92fe79c4e49cefbb
Andoly <andoly.cs@hotmail.com>
2020-11-02 15:47:02 -0300

    Iniciando React-avançado...

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39c126333694e579e75da8485402b4d0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64799b96-cec3-4e7f-9d7b-26e38e9d9ea8'
+};
